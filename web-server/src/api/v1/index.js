bc46bb05308b6a4608762cfae3b18bb8298d76a5
Thomas Smyth <thomas.smyth3141@gmail.com>
2021-01-27 18:15:17 +0000

    Add live endpoint

--- web-server/src/api/v1/index.js
+++ web-server/src/api/v1/index.js
@@ -1,9 +1,11 @@
-import Router from 'koa-router';
+import Router from '@koa/router';
 
 import ping from './ping.js';
+import stats from './live.js';
 
 const router = new Router();
 
 router.post('/ping', ping);
+router.get('/live', stats);
 
 export default router;

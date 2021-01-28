080a6c6addc97668bb2ba4eda96ba14971582737
Thomas Smyth <thomas.smyth3141@gmail.com>
2021-01-28 00:14:14 +0000

    Store plugin data seperately

--- web-server/src/api/v1/ping.js
+++ web-server/src/api/v1/ping.js
@@ -1,45 +1,73 @@
-import { SquadJSInstance } from 'lib/db/models';
+import { SquadJSInstance, SquadJSInstancePlugin } from 'lib/db/models';
+import { Op } from 'lib/db/sequelize';
 
 export default async function (ctx) {
   // Get mandatory variables.
   const host = ctx.request.body.server?.host;
   const queryPort = ctx.request.body.server?.queryPort;
 
-  // Check the mandatoru variables are present.
+  // Check the mandatory variables are present.
   if (!host || !queryPort) {
     ctx.body = { error: 'Invalid ping.' };
     return;
   }
 
   // Collect the other variables.
+  const id = `${host}:${queryPort}`;
   const name = ctx.request.body.server?.name;
   const playerCount = ctx.request.body.server?.playerCount;
 
   const version = ctx.request.body.squadjs?.version;
   const logReaderMode = ctx.request.body.squadjs?.logReaderMode;
 
-  const plugins = JSON.stringify(ctx.request.body.squadjs?.plugins);
+  const plugins = ctx.request.body.squadjs?.plugins;
 
-  // Save ping information.
+  // Save instance information.
   await SquadJSInstance.upsert(
     {
+      id,
       host,
       queryPort,
       name,
       playerCount,
       version,
       logReaderMode,
-      plugins,
       lastPinged: Date.now()
     },
     {
       where: {
-        host,
-        queryPort
+        id
       }
     }
   );
 
-  // respond
+  // Save plugin information.
+  await SquadJSInstancePlugin.bulkCreate(
+    plugins.map(
+      plugin => (
+        {
+          instanceID: id,
+          plugin: plugin.plugin,
+          config: JSON.stringify(plugin)
+        }
+      )
+    ),
+    {
+      updateOnDuplicate: ['config']
+    }
+  );
+
+  await SquadJSInstancePlugin.destroy(
+    {
+      where: {
+        instanceID: id,
+        plugin: {
+          [Op.notIn]: plugins.map(plugin => plugin.plugin)
+        }
+      }
+    }
+  );
+
+  // Respond
   ctx.body = { message: 'Pong.' };
 }
